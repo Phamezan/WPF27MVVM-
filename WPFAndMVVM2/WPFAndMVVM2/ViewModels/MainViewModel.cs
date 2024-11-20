d7df4609e667a46dec7ea6a90653c0eff6341c50
Phamezan <pham2003@outlook.dk>
2024-11-20 08:42:24 +0100

    første comit

---
+++ WPFAndMVVM2/WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private PersonViewModel _selectedPerson;
+
+        public PersonViewModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set 
+            { 
+                _selectedPerson = value;
+                OnPropertyChanged(nameof(SelectedPerson));
+            }
+        }
+
+        public ObservableCollection<PersonViewModel> PersonsVM { get; set; }
+        private PersonRepository personRepo = new PersonRepository();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainViewModel()
+        {
+            PersonsVM = new ObservableCollection<PersonViewModel>();
+            foreach (var person in personRepo.GetAll())
+            {
+                PersonsVM.Add(new PersonViewModel(person));
+            }
+        }
+        public void AddDefaultPerson()
+        {
+           Person person = personRepo.Add("Specify FirstName", "Specify LastName", 0, "Specify Phone");
+           PersonViewModel personViewModel = new PersonViewModel(person);
+           SelectedPerson = personViewModel;
+            PersonsVM.Add(new PersonViewModel(person));
+        }
+    }
+}
---
+++ WPFAndMVVM2/WPFAndMVVM2/Views/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using WPFAndMVVM2.ViewModels;
+
+namespace WPFAndMVVM2
+{
+    public partial class MainWindow : Window
+    {
+        MainViewModel mainViewModel = new MainViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = mainViewModel;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            mainViewModel.AddDefaultPerson();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
